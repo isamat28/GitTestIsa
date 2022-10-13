269e261dca8d15f949d9be64819ad20d2fee1224
isa <isammat@gmail.com>
2022-10-13 18:06:42 +0300

    initial commit

---
+++ src/Deneme1/GitDeneme.java
@@ -0,0 +1,7 @@
+package Deneme1;
+
+public class GitDeneme {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
---
+++ src/GitDeneme2.java
@@ -0,0 +1,5 @@
+public class GitDeneme2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git2");
+    }
+}
